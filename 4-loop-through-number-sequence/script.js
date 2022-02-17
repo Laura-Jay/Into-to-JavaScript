733874c5ee45c5a614c695dcda7e2251a1d0030f
laura J <laura@academy.com>
2022-02-17 17:43:43 +0000

    Bug fixed in 4-loop function

--- 4-loop-through-number-sequence/script.js
+++ 4-loop-through-number-sequence/script.js
@@ -1,9 +1,7 @@
 "use strict" // turns strict mode on
 
-// TODO: fix me
-
 console.log("Teenagers can be:")
 
-for (let ageInYears = 13, ageInYears < 19, ageInYears++) {
+for (let ageInYears = 13; ageInYears < 19; ageInYears++) {
   console.log(`${ageInYears} years old`);
 }
