05d85d01bc342c90f7c6a6d841a254460d8cf355
laura J <laura@academy.com>
2022-02-17 17:31:05 +0000

    initial commit

---
+++ 3-loop-through-string/script.js
@@ -0,0 +1,13 @@
+"use strict"; // turns strict mode on
+
+let word = "LoLlapAlOoZa";
+
+for (let letter of word) {
+  // set capitalised_letter to uppercase of letter
+  let capitalisedLetter = letter.toUpperCase(); // common to use camelCase variable names in JS
+  // print capitalised_letter
+  console.log(capitalisedLetter);
+}
+
+console.log("Easy as that!")
+
